a0195e00e032075d064799c77abd6aafb50cf6ca
Priyanshu <priyanshupandeydelhi@gmail.com>
2018-04-15 17:01:15 +0530

    updated license

--- Install.sh
+++ Install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#
+# Copyright © 2018, "Shenron007" <priyanshu19111999@gmail.com>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
 
 ROOT_UID=0
 
